94e4a20e546f10510dc0059e7c78e37a9e8e29d1
aderizaldi <ade.rizaldi@student.untan.ac.id>
2024-05-20 08:36:30 +0700

    fix: detail agenda statistik peserta

--- resources/views/landing/pages/detail_agenda.blade.php
+++ resources/views/landing/pages/detail_agenda.blade.php
@@ -19,9 +19,9 @@
                     </div>
                     @if ($agenda->formAgendas->count() > 0)
                         <div class="col-lg-12">
-                            <div class="d-flex justify-content-start">
-                                <p class="p-0 m-0 me-3">Kehadiran Peserta: </p>
-                                <div class="d-flex flex-wrap justify-content-start py-3">
+                            <div class="d-flex flex-wrap justify-content-start">
+                                <p class="p-0 m-0 me-2">Kehadiran Peserta: </p>
+                                <div class="d-flex flex-wrap justify-content-start p-2">
                                     @foreach ($status_peserta as $status)
                                         @if ($agenda->formAgendas()->where('status_peserta', $status)->count() > 0)
                                             <span
